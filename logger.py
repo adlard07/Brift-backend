189d59307819209427b0d2e8311f88ec79c2519d
“adlard07” <adelarddcunha@gmail.com>
2025-05-11 11:28:14 +0530

    coded dashboard, added artifacts/all-MiniLM-L6-v2 for sentence encoding, and api router

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.DEBUG,
-    # filename='logs/logs.log',
+    # filename='./objects/logs/logs.log',
     # filemode='w'
 )
 
